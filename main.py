917805bf9eeb691a4e53010392a6b2313706749c
jamshed <jamshhed@vk.ru>
2025-07-24 03:39:15 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import telebot
+import config
+
+bot = telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, text="Привет мой друг в армии! Надеюсь эта штука тебе пригодится, чтобы не сойти с ума и не забыть как нажимат на кнопочки, чтобы получалась магия. Удачи, Джаваскриптизёр!")
+
+
+
+
+
+
+
+bot.infinity_polling()
---
+++ models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Integer, Column, String, Boolean, Text
+from sqlalchemy.orm import DeclarativeBase, Session
+import sqlalchemy
+
+
+engine = sqlalchemy.create_engine("sqlite:///data/DataBase.db")
+
+class Base(DeclarativeBase): pass
+
+class User(Base):
+    __tablename__ = "Users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String, nullable=False)
+    complite_tasks = Column(Text)
+    admin = Column(Boolean, default=False)
+
+Base.metadata.create_all(bind=engine)
+
+
